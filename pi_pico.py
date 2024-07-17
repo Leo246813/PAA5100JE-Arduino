70c844a01c29af446584d9f3815e47eb7dfd9d26
Leoch24 <oyc21@ic.ac.uk>
2024-07-17 11:47:15 +0100

    Update pi_pico.py

--- pi_pico.py
+++ pi_pico.py
@@ -1,40 +1,42 @@
 import time
 from machine import Pin, SPI
+from breakout_paa5100 import BreakoutPAA5100
 
-# SPI0 Pins (Default)
-SCK_PIN = 18
-MOSI_PIN = 19
-MISO_PIN = 16
-CS_PIN = 17
+class SPIInterface:
+    def __init__(self, sck_pin, mosi_pin, miso_pin, cs_pin):
+        self.spi = SPI(0, baudrate=9600, polarity=0, phase=0, bits=8, firstbit=SPI.MSB,
+                       sck=Pin(sck_pin), mosi=Pin(mosi_pin), miso=Pin(miso_pin))
+        self.cs = Pin(cs_pin, Pin.OUT)
 
-# Initialize SPI
-spi = SPI(0, baudrate=9600, polarity=0, phase=0, bits=8, firstbit=SPI.MSB,
-          sck=Pin(SCK_PIN), mosi=Pin(MOSI_PIN), miso=Pin(MISO_PIN))
+    def spi_write_read(self, write_data):
+        self.cs.value(0)  # Select the device
+        read_data = self.spi.readinto(write_data)
+        self.cs.value(1)  # Deselect the device
+        return read_data
 
-# Chip Select (CS) pin as output
-cs = Pin(CS_PIN, Pin.OUT)
+class MotionSensor:
+    def __init__(self):
+        self.flo = BreakoutPAA5100()
+        self.flo.set_rotation(BreakoutPAA5100.DEGREES_0)
+        self.x = 0
+        self.y = 0
 
-# Function to write and read data via SPI
-def spi_write_read(write_data):
-    cs.value(0)  # Select the device
-    read_data = spi.readinto(write_data)
-    cs.value(1)  # Deselect the device
-    return read_data
+    def read_motion(self):
+        delta = self.flo.get_motion()
+        if delta is not None:
+            self.x, self.y = delta
+            print("Relative: x {}, y {}".format(self.x, self.y))
 
-# PAA5100
-from breakout_paa5100 import BreakoutPAA5100 as FlowSensor
+if __name__ == "__main__":
+    # SPI0 Pins (Default)
+    SCK_PIN = 18
+    MOSI_PIN = 19
+    MISO_PIN = 16
+    CS_PIN = 17
 
-flo = FlowSensor()
-flo.set_rotation(FlowSensor.DEGREES_0)
+    spi_interface = SPIInterface(SCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN)
+    motion_sensor = MotionSensor()
 
-# Initial positions
-x = 0
-y = 0
-
-while True:
-    delta = flo.get_motion()
-    if delta is not None:
-        x = delta[0]
-        y = delta[1]
-        print("Relative: x {}, y {}".format(x, y))
-    time.sleep(0.1)
+    while True:
+        motion_sensor.read_motion()
+        time.sleep(0.1)
