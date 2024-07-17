7aa6acef7cf1ffc9d9ebdf4f19eff6d2fecd34ae
Leoch24 <oyc21@ic.ac.uk>
2024-07-17 10:31:56 +0100

    Update pi_pico.py

--- pi_pico.py
+++ pi_pico.py
@@ -1,7 +1,7 @@
 import time
 from machine import Pin, SPI
 
-# SPI0 Pins
+# SPI0 Pins (Default)
 SCK_PIN = 18
 MOSI_PIN = 19
 MISO_PIN = 16
@@ -21,20 +21,15 @@ def spi_write_read(write_data):
     cs.value(1)  # Deselect the device
     return read_data
 
-
-# Pick *one* sensor type by uncommenting the relevant line below:
-
-# PMW3901
-#from breakout_pmw3901 import BreakoutPMW3901 as FlowSensor
-
 # PAA5100
 from breakout_paa5100 import BreakoutPAA5100 as FlowSensor
 
 flo = FlowSensor()
 flo.set_rotation(FlowSensor.DEGREES_0)
 
-tx = 0
-ty = 0
+# Initial positions
+#tx = 0
+#ty = 0
 x = 0
 y = 0
 
@@ -43,8 +38,8 @@ while True:
     if delta is not None:
         x = delta[0]
         y = delta[1]
-        tx += x
-        ty += y
+        #tx += x
+        #ty += y
         #print("Relative: x {}, y {} | Absolute: tx {}, ty {}".format(x, y, tx, ty))
         print("Relative: x {}, y {}".format(x, y))
     time.sleep(0.1)
